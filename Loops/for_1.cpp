c9a68311d253f8b93065ab6cc23150883efaf062
Robert Olieman <robert.e.olieman@gmail.com>
2026-09-10 14:33:00 -0400

    added a loop

--- Loops/for_1.cpp
+++ Loops/for_1.cpp
@@ -3,6 +3,18 @@
 
 int main() {
     for (int i = 0; i < 10; i++) {
-        std::cout << i;
+        std::cout << i << "\n";
+    }
+
+    for (int i = 0; i < 3; i++) {
+        std::string str;
+        if ( i== 0) {
+            str = "zero\n";
+        } else if (i == 1) {
+            str = "one\n";
+        } else if (i == 2) {
+            str = "two\n";
+        }
+        std::cout << str;
     }
 }
