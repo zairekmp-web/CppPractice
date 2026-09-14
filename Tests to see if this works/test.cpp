cb70c84c7b1dcb6624418197af115fb79757306f
Zaire Kemp <Zairekmp@gmail.com>
2026-09-14 13:52:18 -0400

    im practicing this

---
+++ Tests to see if this works/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main() {
+for(int i = 1; i < 100; i++){
+    std::cout << "Zaire loves games.\n";
+    std::cout << "Zaire loves to play cod warzone.\n";
+
+
+
+
+}
+
+
+
+
+
+
+}
