c892f24b9e0844d4dab6d10976692b2efdd58a56
Robert Olieman <robert.e.olieman@gmail.com>
2026-09-23 14:54:54 -0400

    vectors2

---
+++ Vectors/vectors2.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    // TODO 1. Create an empty string vector here
+
+    while (true) {
+        // TODO 2. Allow the user to input a string,
+        // add the input string to the vector,
+        // and print the entire vector
+
+        
+    }
+}
